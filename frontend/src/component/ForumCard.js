4b0a02895c8beb2ce0f87fd355be4dd18e5c04ba
shinyu0430 <0430shinyu@gmail.com>
2021-12-13 22:41:57 +0800

    新增：顯示所有論壇文章

--- frontend/src/component/ForumCard.js
+++ frontend/src/component/ForumCard.js
@@ -14,7 +14,8 @@ import Favorite from "@mui/icons-material/Favorite";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 
-export default function ForumCard() {
+export default function ForumCard(props) {
+  const{title,date,content} = props.article;
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
@@ -32,16 +33,13 @@ export default function ForumCard() {
           />
         </Typography>
         <Typography variant="h5" component="div">
-          「資料夾理財法」懶得記帳也存到錢！
+          {title}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          2021/11/05 22:00
+          {date}
         </Typography>
         <Typography variant="body2">
-          很多盲目存錢的朋友，常常存不到幾萬塊，又轉眼花掉了。都是因為存錢沒有動力的關係！因此，建議您「存錢要有動力」，而且動力越浮誇越好。
-          首先，針對每個存錢的背後設立終極目標，可以是買房子、旅遊基金、買車等等。設好目標後，請再替這個目標取一個華麗而浮誇的名稱。買房基金可以取名為「20坪華而美五臟俱全兩房的張家小豪宅基金」，
-          旅行當然是「2019年日本北海道邊看雪邊泡溫泉邊喝清酒的八天七夜雙人遊」，也可以存一筆「Maserati雙門4座GT跑車……」！（被毆飛XDDD）
-          總而言之，替自己的存錢目標取一個好名字，可以有效增加存錢的動力。花錢之前，先想想那小豪宅基金、日本八天七夜雙人遊等等，也許您會發現這筆錢還是存下來比較好。
+          {content}
         </Typography>
       </CardContent>
       <Typography>
--- frontend/src/view/ForumIndex.js
+++ frontend/src/view/ForumIndex.js
@@ -1,21 +1,64 @@
-import React, { useEffect, useState } from "react";
-import { Box, List, ListItem, ListItemText,Container,Fab} from "@mui/material";
+import React, { useEffect, useState} from "react";
+import { Box,Container,Fab} from "@mui/material";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import AppMenu from "../component/AppMenu";
 import ForumCard from "../component/ForumCard";
 import AddIcon from '@mui/icons-material/Add';
+
+
 export default function ForumIndex() {
+  // const [date, setDate] = React.useState(new Date());
+  const [article, setArticle] = useState([]);
+  useEffect(() => {
+    
+    async function fetchData () {
+      try {
+      const result = await axios.get("/Gossip");
+      console.log(result);
+      setArticle(result.data);
+      }catch(e){
+        // 每有抓取到後端時的預設值
+        setArticle([[
+          {
+          id: 1,
+          title: "ETF 投資心法",
+          user_name: "Andy",
+          user_id: 1,
+          content: "0050,0056是個好投資",
+          date: "2021-12-07"
+          },
+          {
+          id: 2,
+          title: "台積電股票",
+          user_name: "Tim",
+          user_id: 1,
+          content: "台積電近日股市連續上漲",
+          date: "2021-12-06"
+          }
+          ]]);
+      }  
+    }
+    fetchData();
+  },[]);
+
+
+
+ 
+
+
   return (
     <Box>
       <AppMenu />
       <Container maxWidth="sm">
-
+      {article.map((data, index) =>{
+          return <ForumCard key ={index} title={data}></ForumCard>
+      })}
+      
+      {/* <ForumCard></ForumCard>
       <ForumCard></ForumCard>
       <ForumCard></ForumCard>
-      <ForumCard></ForumCard>
-      <ForumCard></ForumCard>
-      <ForumCard></ForumCard>
+      <ForumCard></ForumCard> */}
       </Container>
       <Fab color="primary" aria-label="add"  component={Link} to="../forumAddCard" sx={{
 
