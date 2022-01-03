c3ace6a25218defdca73702ce58734f7bbc65135
cydaf <53554367+cydaf@users.noreply.github.com>
2022-01-03 20:16:39 +0800

    remove unused import

--- backend/src/main/java/com/example/demo/Controller/AccountController.java
+++ backend/src/main/java/com/example/demo/Controller/AccountController.java
@@ -2,13 +2,9 @@ package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.sql.SQLException;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.dao.AccountDAO;
 import com.example.demo.entity.Account;
